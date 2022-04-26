3f031cc6094afeef9504c6b2fb1a7a7956313cb1
MG2496 <michaelgamboamoya24@gmail.com>
2022-04-26 10:07:02 -0600

    completed

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,20 +1,40 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Number from "./Number";
 
-export default Game = ({ randomNumbers }) => {
+export default Game = ({ randomNumbersCount }) => {
+    const [randomNumbers, setRandomNumbers ] = useState([]);
+    const [ target, setTarget ] = useState(0);
+    const [ selectedNumbers, setSelectedNumbers ] = useState([]);
+    
     //const target = 10 + Math.floor(40 * Math.random());
 
-    const numbers = Array.from({ length: randomNumbers }).map(() => 1 + Math.floor(10 * Math.random()));
-    const target = numbers.slice(0, randomNumbers -2).reduce( (acc, cur) => acc + cur, 0);
+    //const numbers = Array.from({ length: randomNumbers }).map(() => 1 + Math.floor(10 * Math.random()));
+    //const target = numbers.slice(0, randomNumbers -2).reduce( (acc, cur) => acc + cur, 0);
+
+    //const [selectedNumbers, setSelectedNumbers] = useState([]);
+
+    useEffect(() => {
+        const numbers = Array.from({ length: randomNumbersCount }).map(() => 1 + Math.floor(10 * Math.random()));
+        const target = numbers.slice(0, randomNumbersCount -2).reduce( (acc, cur) => acc + cur, 0); 
+        setRandomNumbers(numbers);
+        setTarget(target);
+    }, []);
+
+    const isNumberSelected = numberIndex => selectedNumbers.some(number => number === numberIndex);
+
+    const selectNumber = number => setSelectedNumbers ([...selectedNumbers, number]);
+
 
     return (
         <View>
             <Text style={styles.target}>{target}</Text>
             <View style={styles.randomContainer}>
-            {numbers.map((number, index) => (
-                <Text key={index} style={styles.random}>{number}</Text>
+            {randomNumbers.map((number, index) => (
+                <Number key={index} id={index} number={number} isSelected={isNumberSelected(index)} onSelected={selectNumber} />
+                
             ))}
-        </View>
+            </View>
         </View>
     );
 };
@@ -34,15 +54,15 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
     },
 
-    random: {
-        backgroundColor: 'green',
-        color: 'white',
-        width: 100,
-        marginHorizontal: 15,
-        marginVertical: 25,
-        fontSize: 35,
-        textAlign: 'center',
-    }
+    // random: {
+    //     backgroundColor: 'green',
+    //     color: 'white',
+    //     width: 100,
+    //     marginHorizontal: 15,
+    //     marginVertical: 25,
+    //     fontSize: 35,
+    //     textAlign: 'center',
+    // }
 
 
 });
